afab36fe83a9a4b9b80d1f12d0b2de53e2097dd5
Narahari Anuganti <nanuganti@ebsco.com>
2022-04-05 18:17:11 -0400

    feat: add express

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const processFizzBuzz = require("./helloFizzBuzz");
+const express = require("express");
+const app = express();
+const port = 3010;
 
-const outPut = processFizzBuzz([1, 3, 5, "", 15, "A", 23]);
-console.log(outPut);
+app.use("/getfizzbuzz", (req, res) => {
+
+    res.send("hello world");
+});
+
+app.listen(port, () => {
+    console.log("listening on 1010 port");
+})
+
+// const processFizzBuzz = require("./helloFizzBuzz");
+
+// const outPut = processFizzBuzz([1, 3, 5, "", 15, "A", 23]);
+// console.log(outPut);
